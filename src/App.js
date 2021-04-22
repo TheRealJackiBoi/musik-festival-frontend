048ac02302b25855996cd8013f86b38209b1e045
Christian Schwenger <drikdrok02@gmail.com>
2021-04-22 15:01:26 +0200

    Voting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {Component} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import {Chat} from './components/Chat';
 import {VideoPlayer} from './components/VideoPlayer';
 import {Voting} from './components/Voting';
@@ -15,14 +15,31 @@ const auth = firebase.auth();
 
 export function App() {    
   const [user] = useAuthState(auth);
+  const [video, setVideo] = useState({});
+  const [videoIndex, setVideoIndex] = useState(0);
+
+
+  useEffect(() => {
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&id=dQw4w9WgXcQ&key=AIzaSyABQyO1Hrn0HCqGgRJFZm-Wm4fqNRFcjO4&index=" + videoIndex)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          result.items[0].snippet.title = result.items[0].snippet.title.replace("(Video)", "");
+          setVideo(result.items[0].snippet)
+        },
+      )
+  }, [])
+
+
     return (
       <div className="App">
           
-        <div id="video-vote">
-          <VideoPlayer />
-          <VideoInfo video={{title: "Never gonna give you up"}} viewers={"127.099.233"} />
+          <div id="video-vote">
+            <VideoPlayer />
+            <VideoInfo video={video} viewers={"127.099.233"} />
+          </div>
           <Voting />
-        </div>
 
         
           <Chat user={user} auth={auth} db={db} />
